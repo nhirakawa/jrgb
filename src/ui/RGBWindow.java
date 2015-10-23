e8d8f1466499feae5b672038bdf0ecc66efd0c24
Nick Hirakawa <nh0815@gmail.com>
2015-10-22 22:45:15 -0400

    Only show image files when choosing files

--- src/ui/RGBWindow.java
+++ src/ui/RGBWindow.java
@@ -7,6 +7,9 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.File;
+import javax.swing.*;
+import javax.swing.filechooser.*;
 
 /**
  * Created by nick on 10/22/15.
@@ -42,6 +45,7 @@ public class RGBWindow extends JFrame{
         @Override
         public void actionPerformed(ActionEvent actionEvent) {
             JFileChooser chooser = new JFileChooser();
+            chooser.setFileFilter(new ImageFilter());
             int returnVal = chooser.showOpenDialog(null);
 
             if(returnVal == JFileChooser.APPROVE_OPTION){
@@ -54,6 +58,56 @@ public class RGBWindow extends JFrame{
                 }
             }
         }
+    }
 
+    private static class ImageFilter extends FileFilter {
+
+        public boolean accept(File f) {
+            if (f.isDirectory()) {
+                return true;
+            }
+
+            String extension = Utils.getExtension(f);
+            if (extension != null) {
+                if (extension.equals(Utils.tiff) ||
+                        extension.equals(Utils.tif) ||
+                        extension.equals(Utils.gif) ||
+                        extension.equals(Utils.jpeg) ||
+                        extension.equals(Utils.jpg) ||
+                        extension.equals(Utils.png)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            return false;
+        }
+        public String getDescription() {
+            return "Just Images";
+        }
+    }
+
+    private static class Utils {
+
+        public final static String jpeg = "jpeg";
+        public final static String jpg = "jpg";
+        public final static String gif = "gif";
+        public final static String tiff = "tiff";
+        public final static String tif = "tif";
+        public final static String png = "png";
+
+        /*
+         * Get the extension of a file.
+         */
+        public static String getExtension(File f) {
+            String ext = null;
+            String s = f.getName();
+            int i = s.lastIndexOf('.');
+
+            if (i > 0 &&  i < s.length() - 1) {
+                ext = s.substring(i+1).toLowerCase();
+            }
+            return ext;
+        }
     }
 }
