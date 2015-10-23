bdea22106ab7f07008e56de1df3e416aab978349
Nick Hirakawa <nh0815@gmail.com>
2015-10-22 21:14:32 -0400

    Add constructor in RGBWindow

--- src/ui/RGBWindow.java
+++ src/ui/RGBWindow.java
@@ -1,7 +1,14 @@
 package ui;
 
+import javax.swing.*;
+
 /**
  * Created by nick on 10/22/15.
  */
-public class RGBWindow {
+public class RGBWindow extends JFrame{
+
+
+    public RGBWindow(){
+        super("jrgb");
+    }
 }
