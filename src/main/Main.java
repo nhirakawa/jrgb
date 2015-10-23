ba82b7c57307c9de221806062314b40c7ad58675
Nick Hirakawa <nh0815@gmail.com>
2015-10-22 22:22:21 -0400

    Create RGBWindow when app starts

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,13 @@
 package main;
 
+import ui.RGBWindow;
+
 /**
  * Created by nick on 10/22/15.
  */
 public class Main {
 
     public static void main(String[] args){
-
+        new RGBWindow();
     }
 }
