dcc9020438fa9767fae1d4dd9cafec39328363db
Nick Hirakawa <nh0815@gmail.com>
2015-10-22 21:38:32 -0400

    Add skeleton for RGBPanel

--- src/ui/RGBPanel.java
+++ src/ui/RGBPanel.java
@@ -1,9 +1,31 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * Created by nick on 10/22/15.
  */
 public class RGBPanel extends JPanel {
+
+    private BufferedImage image;
+
+    public RGBPanel(){
+        super();
+        image = new BufferedImage(10, 10, BufferedImage.TYPE_3BYTE_BGR);
+    }
+
+    public void setImage(BufferedImage img){
+        this.image = img;
+        invalidate();
+        repaint();
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, null);
+    }
+
 }
