b75a7c918a44a2e124b3286d6c491a0bfa42a716
Nick Hirakawa <nh0815@gmail.com>
2015-10-24 22:54:10 -0400

    Remove comments

--- src/rgb/EightBitRGBStrategy.java
+++ src/rgb/EightBitRGBStrategy.java
@@ -17,21 +17,4 @@ public class EightBitRGBStrategy extends RGBStrategy {
         return new Color(red, green, blue).getRGB();
     }
 
-    /*@Override
-    public Color convert(Color c) {
-        int red = (int)Math.floor(c.getRed() / 32) * 32;
-        int green = (int)Math.floor(c.getGreen() / 32) * 32;
-        int blue = (int)Math.floor(c.getBlue() / 64) * 64;
-        return new Color(red, green, blue);
-    }
-
-    @Override
-    public BufferedImage convert(BufferedImage image) {
-        BufferedImage result = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
-        for(int i = 0; i < image.getWidth(); i++){
-            for(int j = 0; j < image.getHeight(); j++){
-
-            }
-        }
-    }*/
 }
